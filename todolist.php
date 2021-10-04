075453e4256ba6ee352bb3fd144a70ea385a68ae
UNE BRUNE <83824371+unebrune@users.noreply.github.com>
2021-10-04 23:28:37 +0200

    Create todolist.php

---
+++ todolist.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang= "en">
+   <head>
+    <meta charset="UTF-8">
+    <title> Home </title>
+    <link rel="stylesheet" href="style.css">
+    <script src="myscripts.php"></script> 
+</head>
+   <body>
+       <div id="carte">
+    <h1> TODOList's Youssra </h1>
+    <h2> Here are the card with the task lists to be done </h2>
+        <form>
+        
+            <input type="text" placeholder="Add your new todo" class="inputField">
+            <button class="todo-button" type="submit">
+                +
+</button>
+</form>
+<div id="cartes">
+<ul class ="todoList">
+    <li> JUST FIND BELOW THE TASKS TO DO !  <button>I DID IT ALL :)</button></li>
+</div>
+</ul>
+
+<script src="./script.js"></script>
+</body>
+</html>
